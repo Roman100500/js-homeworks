7f449e14403a3a6a31019c70935476da44650b0e
Roman100500 <chizhkinra001@mail.ru>
2023-03-17 23:18:54 +0300

    Homework1 complete

---
+++ README.md
@@ -0,0 +1,6 @@
+## JavaScript
+
+### Полезные данные:
+
+- [Современный язык JavaScript](https://learn.javascript.ru)
+- [Учебные материалы JavaScript от Mozilla](https://developer.mozilla.org/ru/docs/Web/JavaScript)
---
+++ homework_1/js/script.js
@@ -0,0 +1,67 @@
+/* 
+const message = "Hello, World";
+console.log(message);
+*/
+
+/* const a = 5;
+const b = 5;
+console.log(a + b);
+console.log(a - b);
+*/
+
+/* const x = "5";
+const y = 5;
+console.log(x + y);
+console.log(x - y);
+*/
+
+/* const m = "ber";
+const n = 56;
+console.log(m + n);
+console.log(m - n);
+*/
+
+/*
+let p = 5;
+p = "Ivan";
+console.log(p);
+p = 6;
+console.log(p);
+p = true;
+console.log(p);
+*/
+
+/* 
+let a;
+a = 6;
+console.log(a / 0);
+*/
+
+/* 
+let a;
+console.log(a);
+*/
+
+/* 
+let a = 5;
+if(true) {
+    let a = 6;
+}
+console.log(a);
+*/
+
+/* 
+var a = 5;
+if(true) {
+    var a = 6;
+}
+console.log(a);
+*/
+/* 
+console.log(typeof null); // это ошибка в языке, на самом деле это не объект.
+*/
+
+/* 
+let isGreater = 4 > 1;
+console.log(isGreater);
+*/
