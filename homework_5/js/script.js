e1505e8fbd17585ff005201fd6e44bfed9f4d637
Roman100500 <chizhkinra001@mail.ru>
2023-03-31 23:50:49 +0300

    Homework5 updated 2

--- homework_5/js/script.js
+++ homework_5/js/script.js
@@ -60,20 +60,20 @@ ${post.comments[1].text}`);
 
 /* 
 const products = [
-{
-id: 3,
-price: 200,
-},
-{
-id: 4,
-price: 900,
-},
-{
-id: 1,
-price: 1000,
-},
+  {
+    id: 3,
+    price: 200,
+  },
+  {
+    id: 4,
+    price: 900,
+  },
+  {
+    id: 1,
+    price: 1000,
+  },
 ];
-products.forEach(elem => elem.price *=0.85);
+products.forEach((elem) => (elem.price *= 0.85));
 console.log(products);
 */
 
