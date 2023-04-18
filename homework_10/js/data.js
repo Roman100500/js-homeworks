6dbb5d67549fc3cfd13ce0d12a98a4485372cf98
Roman100500 <chizhkinra001@mail.ru>
2023-04-18 22:39:35 +0300

    Homework10 complete

---
+++ homework_10/js/data.js
@@ -0,0 +1,20 @@
+export const personData = `[
+  {
+    "name": "Roman",
+    "age": "32",
+    "profession": "driver",
+    "url": "https://doska.berlin/files/upload/psk1473829854_a.jpg"
+  },
+  {
+    "name": "Polina",
+    "age": "25",
+    "profession": "teacher",
+    "url": "https://www.nvgazeta.ru/upload/resize_cache/iblock/5c9/345_9999_0/5c94ab1f5eae01c186d8fa6d91076341.jpg"
+  },
+  {
+    "name": "Alex",
+    "age": "34",
+    "profession": "programmer",
+    "url": "https://layboard.com/images/uploads/119666/300x300_itspec1.jpg"
+  }
+]`;
