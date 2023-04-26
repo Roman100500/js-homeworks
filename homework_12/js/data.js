d035bcd395e5af18d77f2103b57fe1f729c9e09a
Roman100500 <chizhkinra001@mail.ru>
2023-04-26 13:53:25 +0300

    Homework12 complete

---
+++ homework_12/js/data.js
@@ -0,0 +1,50 @@
+export const dataProducts = `[
+    {
+    "name": "ellery x&nbsp;m&rsquo;o capsule",
+    "image": "img/product-photo-1.png",
+    "price": 52,
+    "color": "Red",
+    "size": "Xl",
+    "quantity": 1
+    },
+    {
+    "name": "ellery x&nbsp;m&rsquo;o capsule",
+    "image": "img/product-photo-2.png",
+    "price": 52,
+    "color": "Black",
+    "size": "Xl",
+    "quantity": 1
+    },
+    {
+    "name": "ellery x&nbsp;m&rsquo;o capsule",
+    "image": "img/product-photo-3.png",
+    "price": 52,
+    "color": "Green",
+    "size": "XXXl",
+    "quantity": 1
+    },
+    {
+    "name": "ellery x&nbsp;m&rsquo;o capsule",
+    "image": "img/product-photo-4.png",
+    "price": 52,
+    "color": "Blue",
+    "size": "L",
+    "quantity": 1
+    },
+    {
+    "name": "ellery x&nbsp;m&rsquo;o capsule",
+    "image": "img/product-photo-5.png",
+    "price": 52,
+    "color": "Black",
+    "size": "Xl",
+    "quantity": 1
+    },
+    {
+    "name": "ellery x&nbsp;m&rsquo;o capsule",
+    "image": "img/product-photo-6.png",
+    "price": 52,
+    "color": "Black",
+    "size": "S",
+    "quantity": 1
+    }
+]`;
